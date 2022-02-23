a9ac75bd3a8f6e7328fc7e16f35ed0e30fbdf2b4
M.ZELMATI <100286013+M-ZELMATI@users.noreply.github.com>
2022-02-23 22:21:02 +0100

    Update CalculRemot.java

--- RMI Client2/src/Service/CalculRemot.java
+++ RMI Client2/src/Service/CalculRemot.java
@@ -2,16 +2,9 @@ package Service;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-
-import javax.swing.JLabel;
-import java.awt.Dimension;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public interface CalculRemot extends Remote{
 	public double calcul(double chiffre1,double chiffre2,String operateur) throws RemoteException;
