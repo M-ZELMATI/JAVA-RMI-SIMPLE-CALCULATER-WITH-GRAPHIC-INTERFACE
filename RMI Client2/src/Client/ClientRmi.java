78b73ffceb83a2f2d2eafab9aa5c06a223daeea4
M.ZELMATI <100286013+M-ZELMATI@users.noreply.github.com>
2022-02-23 22:15:46 +0100

    Update ClientRmi.java

--- RMI Client2/src/Client/ClientRmi.java
+++ RMI Client2/src/Client/ClientRmi.java
@@ -19,41 +19,45 @@ import javax.swing.JPanel;
 import Service.CalculRemot;
 
 	public class ClientRmi extends JFrame {
-		
-
 		  private JPanel container = new JPanel();
-		  //Tableau stockant les éléments à afficher dans la calculatrice
+		  //Tableau stockant les Ã©lÃ©ments Ã  afficher dans la calculatrice
 		  String[] tab_string = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "=", "C", "+", "-", "*", "/",};
-		  //Un bouton par élément à afficher
+		  //Un bouton par Ã©lÃ©ment Ã  afficher
 		  JButton[] tab_button = new JButton[tab_string.length];
 		  private JLabel ecran = new JLabel();
 		  private Dimension dim = new Dimension(50, 40);
-		  private Dimension dim2 = new Dimension(50, 31);
+		  private Dimension dim2 = new Dimension(50, 29);
 		  private double chiffre1;
 		  private boolean clicOperateur = false, update = true;
 		  private String operateur = "";
+	      Double T[]=new Double[2];
+	      
 		  private CalculRemot stub;
+		  
 		  public ClientRmi(){
-				
-
 		    this.setSize(250, 350);
 		    this.setTitle("Calculette");
 		    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		    this.setLocationRelativeTo(null);
 		    this.setResizable(false);
-		    //On initialise le conteneur avec tous les composants
+		    //On initialise le conteneur 
 		      initComposant();
-		      //On ajoute le conteneur
+		    //On ajoute le conteneur
 		    this.setContentPane(container);
 		    this.setVisible(true);
+		    try {
+				stub=(CalculRemot)Naming.lookup("rmi://localhost:1099/CL") ;
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		  }
-
 		  private void initComposant(){
-		    //On définit la police d'écriture à utiliser
+		    //On dÃ©finit la police d'Ã©criture Ã  utiliser
 		    Font police = new Font("Arial", Font.BOLD, 20);
 		    ecran = new JLabel("0");
 		    ecran.setFont(police);
-		    //On aligne les informations à droite dans le JLabel
+		    //On aligne les informations Ã  droite dans le JLabel
 		    ecran.setHorizontalAlignment(JLabel.RIGHT);
 		    ecran.setPreferredSize(new Dimension(220, 20));
 		    JPanel operateur = new JPanel();      
@@ -62,23 +66,12 @@ import Service.CalculRemot;
 		    chiffre.setPreferredSize(new Dimension(165, 225));
 		    JPanel panEcran = new JPanel();
 		    panEcran.setPreferredSize(new Dimension(220, 30));
-		    try {
-				CalculRemot stub=(CalculRemot)Naming.lookup("rmi://localhost:1099/CL") ;
-		    	
-			} catch (Exception e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-				
-			} 
-		    //On parcourt le tableau initialisé
-		    //afin de créer nos boutons
+		    //On parcourt le tableau initialisÃ©
+		    //afin de crÃ©er nos boutons
 		    for(int i = 0; i < tab_string.length; i++){
 		      tab_button[i] = new JButton(tab_string[i]);
 		      tab_button[i].setPreferredSize(dim);
 		      switch(i){
-		        //Pour chaque élément situé à la fin du tableau
-		        //et qui n'est pas un chiffre
-		        //on définit le comportement à avoir grâce à un listener
 		        case 11 :
 		          tab_button[i].addActionListener(new EgalListener());
 		          chiffre.add(tab_button[i]);
@@ -109,8 +102,6 @@ import Service.CalculRemot;
 		          operateur.add(tab_button[i]);
 		          break;
 		        default :
-		          //Par défaut, ce sont les premiers éléments du tableau
-		          //donc des chiffres, on affecte alors le bon listener
 		          chiffre.add(tab_button[i]);
 		          tab_button[i].addActionListener(new ChiffreListener());
 		          break;
@@ -120,48 +111,28 @@ import Service.CalculRemot;
 		    panEcran.setBorder(BorderFactory.createLineBorder(Color.black));
 		    container.add(panEcran, BorderLayout.NORTH);
 		    container.add(chiffre, BorderLayout.CENTER);
-		    container.add(operateur, BorderLayout.EAST);
-		   
-		    
+		    container.add(operateur, BorderLayout.EAST); 
 		  }
-
-		  //Méthode permettant d'effectuer un calcul selon l'opérateur sélectionné
-		 
-
-		  //Listener utilisé pour les chiffres
-		  //Permet de stocker les chiffres et de les afficher
-		  
+		  //Listener utilisÃ© pour les chiffres
 		  class ChiffreListener implements ActionListener {
 		    public void actionPerformed(ActionEvent e){
 		      //On affiche le chiffre additionnel dans le label
 		    	 String str = ((JButton)e.getSource()).getText();
-		    	 System.out.println(str);
-		    	 String T[] =new String[2];
-		    	  
+		    	 String T[] =new String[2]; 	  
 				try {
-					CalculRemot stub=(CalculRemot)Naming.lookup("rmi://localhost:1099/CL") ;
-                    T=stub.ChiffreListener(update, ecran.getText() , str);
-                  
+                    T=stub.ChiffreListener(update, ecran.getText() , str);  
 					ecran.setText(T[0]);
 					update=T[1].equals("1")?true:false;
 				} catch (Exception e2) {
 					// TODO Auto-generated catch block
 					e2.printStackTrace();
-				}
-			    
-
-		      
-		    	  
-				  
-			
+				} 
 		    }
 		  }
-
-		  //Listener affecté au bouton =
+		  //Listener affectÃ© au bouton =
 		  class EgalListener implements ActionListener {
 		    public void actionPerformed(ActionEvent arg0){
 		      try {
-				CalculRemot stub=(CalculRemot)Naming.lookup("rmi://localhost:1099/CL") ;
 				if(clicOperateur){
 				   chiffre1=stub.calcul(chiffre1, Double.valueOf(ecran.getText()).doubleValue(), operateur);
 				   ecran.setText(""+chiffre1);
@@ -177,45 +148,46 @@ import Service.CalculRemot;
 		    }
 		  }
 
-		  //Listener affecté au bouton +
+		  
+		  
+		  
+		  
+		  //Listener affectÃ© au bouton +
 		  class PlusListener implements ActionListener {
 		    public void actionPerformed(ActionEvent arg0){
-		    	Double T[]=new Double[2];
 		    	try {
-					CalculRemot stub=(CalculRemot)Naming.lookup("rmi://localhost:1099/CL") ;
 		            double chiffre2=Double.parseDouble(ecran.getText());
 		            chiffre1=clicOperateur?chiffre1:chiffre2;
-		            T=stub.OperateurListener(clicOperateur, chiffre1, chiffre2, operateur);
-		            chiffre1=T[0];
-		            if(clicOperateur) 
-					  ecran.setText(String.valueOf(T[0]));
-		              clicOperateur=(T[1]==1.0)?true:false;
-				      operateur = "+";
-				      update = true;
+		            if(clicOperateur) {
+			               chiffre1 =stub.calcul(chiffre1, chiffre2, operateur);
+			               ecran.setText(String.valueOf(chiffre1));
+			            }
+		            else {
+			            clicOperateur=true;
+		            }
+				    operateur = "+";
+				    update = true;   
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 					System.out.println("call");
-				}
-		      
+				}  
 		    }
 		  }
-
-		  //Listener affecté au bouton -
+		  //Listener affectÃ© au bouton -
 		  class MoinsListener implements ActionListener {
 		    public void actionPerformed(ActionEvent arg0){
-		    	Double T[]=new Double[2];
 		    	try {
-					CalculRemot stub=(CalculRemot)Naming.lookup("rmi://localhost:1099/CL") ;
 		            double chiffre2=Double.parseDouble(ecran.getText());
 		            System.out.println(chiffre2);
 		            chiffre1=clicOperateur?chiffre1:chiffre2;
-		            T=stub.OperateurListener(clicOperateur, chiffre1, chiffre2, operateur);
-		            System.out.println("T[0]:"+T[0]);
-		            chiffre1=T[0];
-		            if(clicOperateur) 
-					  ecran.setText(String.valueOf(T[0]));
-		            clicOperateur=(T[1]==1.0)?true:false;
+		            if(clicOperateur) {
+			               chiffre1 =stub.calcul(chiffre1, chiffre2, operateur);
+			               ecran.setText(String.valueOf(chiffre1));
+			            }
+			            else {
+			            	clicOperateur=true;
+			            }
 				      operateur = "-";
 				      update = true;
 				} catch (Exception e) {
@@ -225,24 +197,22 @@ import Service.CalculRemot;
 				}
 		    }
 		  }
-
-		  //Listener affecté au bouton *
+		  //Listener affectÃ© au bouton *
 		  class MultiListener implements ActionListener {
 		    public void actionPerformed(ActionEvent arg0){
-		    	Double T[]=new Double[2];
 		    	try {
-					CalculRemot stub=(CalculRemot)Naming.lookup("rmi://localhost:1099/CL") ;
 		            double chiffre2=Double.parseDouble(ecran.getText());
 		            System.out.println(chiffre2);
 		            chiffre1=clicOperateur?chiffre1:chiffre2;
-		            T=stub.OperateurListener(clicOperateur, chiffre1, chiffre2, operateur);
-		            System.out.println("T[0]:"+T[0]);
-		            chiffre1=T[0];
-		            if(clicOperateur) 
-					  ecran.setText(String.valueOf(T[0]));
-		            clicOperateur=(T[1]==1.0)?true:false;
-				      operateur = "*";
-				      update = true;
+		            if(clicOperateur) {
+		               chiffre1 =stub.calcul(chiffre1, chiffre2, operateur);
+		               ecran.setText(String.valueOf(chiffre1));
+		            }
+		            else {
+		            	clicOperateur=true;
+		            }
+				    operateur = "*";
+				    update = true;
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -251,23 +221,22 @@ import Service.CalculRemot;
 		    }
 		  }
 
-		  //Listener affecté au bouton /
+		  //Listener affectÃ© au bouton /
 		  class DivListener implements ActionListener {
 		    public void actionPerformed(ActionEvent arg0){
-		    	Double T[]=new Double[2];
 		    	try {
-					CalculRemot stub=(CalculRemot)Naming.lookup("rmi://localhost:1099/CL") ;
 		            double chiffre2=Double.parseDouble(ecran.getText());
 		            System.out.println(chiffre2);
 		            chiffre1=clicOperateur?chiffre1:chiffre2;
-		            T=stub.OperateurListener(clicOperateur, chiffre1, chiffre2, operateur);
-		            System.out.println("T[0]:"+T[0]);
-		            chiffre1=T[0];
-		            if(clicOperateur) 
-					  ecran.setText(String.valueOf(T[0]));
-		            clicOperateur=(T[1]==1.0)?true:false;
-				      operateur = "/";
-				      update = true;
+		            if(clicOperateur) {
+			               chiffre1 =stub.calcul(chiffre1, chiffre2, operateur);
+			               ecran.setText(String.valueOf(chiffre1));
+			         }
+			         else {
+			            	clicOperateur=true;
+			         }
+				    operateur = "/";
+				    update = true;
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -275,8 +244,7 @@ import Service.CalculRemot;
 				}
 		    }
 		  }
-
-		  //Listener affecté au bouton de remise à zéro
+		  //Listener affectÃ© au bouton de remise Ã  zÃ©ro
 		  class ResetListener implements ActionListener {
 		    public void actionPerformed(ActionEvent arg0){
 		      clicOperateur = false;
@@ -286,14 +254,10 @@ import Service.CalculRemot;
 		      ecran.setText("");
 		    }
 		  }      
-
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
-			
+		try {	
 			ClientRmi CR=new ClientRmi();
-			
 		}  catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
